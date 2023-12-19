245825b881a2b258161637a476b9c53e1b654d33
Mendepoojitha123 <153059453+Mendepoojitha123@users.noreply.github.com>
2023-12-19 15:19:32 +0530

    Add files via upload
    my release is completed

---
+++ src/test/java/com/release1/Class1.java
@@ -0,0 +1,11 @@
+package com.release1;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("i am in a test application release team");
+
+	}
+
+}
